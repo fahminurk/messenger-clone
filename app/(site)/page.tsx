046610397627f5fc3b190553fc5af69332e27ff8
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-15 22:15:23 +0700

    auth

---
+++ app/(site)/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import AuthForm from "./components/authForm";
+
+const Auth = () => {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+          alt="Logo"
+        />
+      </div>
+      {/*  */}
+      <AuthForm />
+    </div>
+  );
+};
+
+export default Auth;
